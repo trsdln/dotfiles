392001437fc4bf09d8f14f6d273b4908cb515d82
Taras Dolynyuk <trsdln@gmail.com>
2019-08-11 16:29:53 +0300

    added visudo notes

--- linux/post-install.sh
+++ linux/post-install.sh
@@ -1,5 +1,10 @@
 #/bin/sh
 
+# enable to run basic commands for user
+# sudo visudo
+# append this
+# taras ALL=NOPASSWD:/usr/bin/zzz,/usr/bin/ZZZ,/usr/bin/shutdown
+
 # enable DHCP
 ln -s /etc/sv/dhcpcd /var/service/
 
