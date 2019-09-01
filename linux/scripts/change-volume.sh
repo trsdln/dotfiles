7112eac419c0d07188f103de4b649431a09a7415
Taras Dolynyuk <trsdln@gmail.com>
2019-09-01 21:24:20 +0300

    Fix volume status notification

--- linux/scripts/change-volume.sh
+++ linux/scripts/change-volume.sh
@@ -7,7 +7,7 @@ SOURCE=$(amixer scontrols | cut -d "'" -f2 | head -n 1)
 amixer sset "${SOURCE}" "${VOLUME_VALUE}"
 
 VOLUME_STATUS=$(amixer get "${SOURCE}" | tail -n 1)
-NEW_VOLUME_VALUE=$(echo $VOLUME_STATUS | awk -F'[][]' '{print $2 " " $6}')
+NEW_VOLUME_VALUE=$(echo $VOLUME_STATUS | awk -F'[][]' '{print $2 " " $4}')
 
 notify-send --hint string:x-canonical-private-synchronous:volume \
   --icon audio-volume-high-symbolic \
