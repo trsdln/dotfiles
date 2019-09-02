e7d865babb9579a3aae98a4c1005f93959e588c1
Taras Dolynyuk <trsdln@gmail.com>
2019-09-03 01:09:04 +0300

    Improved status line date

--- linux/scripts/statusline-update.sh
+++ linux/scripts/statusline-update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-LOCALTIME=$(date +%H:%M)
+LOCALTIME=$(date +%H:%M%4a%_d-%b)
 
 TLP_RES=$(sudo tlp-stat -b | grep -E '(Charge\s+=|BAT0/status)')
 BAT_STATUS_FLAG=$([ $(echo $TLP_RES | awk '{print $3}') = $BAT_DISCH_STATUS ] && echo '' || echo '+')
 BAT=$(echo $TLP_RES  | awk '{print $6 "%"}')
 
-xsetroot -name "| $BAT_STATUS_FLAG $BAT | $LOCALTIME |"
+xsetroot -name " $BAT_STATUS_FLAG $BAT | $LOCALTIME "
