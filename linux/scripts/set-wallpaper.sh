f3679128a9269eae2272076ef0ea0aa2b4180746
Taras Dolynyuk <trsdln@gmail.com>
2019-09-06 00:26:49 +0300

    Improve set wallpaper handling

--- linux/scripts/set-wallpaper.sh
+++ linux/scripts/set-wallpaper.sh
@@ -1,10 +1,7 @@
 #!/bin/dash
 
 # Pass file name as first argument to this script
-
 NEW_WALLPAPER=$1
 
-# todo: handle different extensions
-cp $NEW_WALLPAPER ~/Pictures/wallpaper.jpg
-
-feh --bg-scale ~/Pictures/wallpaper.jpg
+feh --bg-scale "${NEW_WALLPAPER}"
+notify-send "Wallpaper Changed" "${NEW_WALLPAPER}"
