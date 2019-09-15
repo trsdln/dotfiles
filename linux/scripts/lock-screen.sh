380dc4ab512fbd32c4ec1f5314ff04321a27cf8d
Taras Dolynyuk <trsdln@gmail.com>
2019-09-15 12:06:01 +0300

    Improve lock screen

--- linux/scripts/lock-screen.sh
+++ linux/scripts/lock-screen.sh
@@ -1,5 +1,9 @@
 #!/bin/dash
 
+# otherwise we get status line content of status line before lock/sleep
+# over next statusline refresh timeout
+statusline-update.sh
+
 # we need to change keyboard layout to correct one otherwise
 # there is no ability to change it while slock is running
 setxkbmap -model thinkpad -layout us
