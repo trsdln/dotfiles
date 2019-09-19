1db2eaf38be98969d6d5630209d124f09167ed33
Taras Dolynyuk <trsdln@gmail.com>
2019-09-19 22:24:58 +0300

    Added weather to status line

--- linux/scripts/statusline-update.sh
+++ linux/scripts/statusline-update.sh
@@ -3,6 +3,8 @@
 LOCAL_TIME=$(date '+%a %d %b %H:%M')
 NY_TIME="" # "• $(TZ='America/New_York' date '+%a %H:%M')"
 
+
+# Battery status
 BAT_NAME="BAT0"
 BAT_CAPACITY=$(cat "/sys/class/power_supply/$BAT_NAME/capacity")
 BAT_STATUS=$(cat "/sys/class/power_supply/$BAT_NAME/status")
@@ -31,12 +33,44 @@ if [ $BAT_STATUS = 'Full' ]; then
   STATUS_ICON='⚡'
 fi
 
+
+# CPU temperature
 CPU_TEMP=$(sensors | awk '/Core 0/ {print $3}')
 
+
+# Network status
 WIRED_NET_STATUS=$(cat /sys/class/net/e*/operstate)
 WIRELESS_NET_STATUS=$(cat /sys/class/net/w*/operstate)
 [ "${WIRED_NET_STATUS}" = 'down' -a "${WIRELESS_NET_STATUS}" = 'down' ] && NETWORK_STATUS="❎" || NETWORK_STATUS="🌐"
 
+
+# Keyboard layout
 KEYBOARD_LAYOUT="$(setxkbmap -query | awk '/layout/ {print toupper($2)}')"
 
-xsetroot -name " ${NETWORK_STATUS} • ${CPU_TEMP} • ${STATUS_ICON} ${BAT_CAPACITY}% • ${KEYBOARD_LAYOUT} • ${LOCAL_TIME} ${NY_TIME}"
+
+# Weather
+WEATHER_FILE="$HOME/.local/share/weatherreport"
+WEATHER_CACHE="$HOME/.local/share/weatherreport.cache"
+
+getforecast() {
+  rm -f "${WEATHER_FILE}"
+  ping -q -c 1 1.1.1.1 >/dev/null && curl -s "wttr.in" > "${WEATHER_FILE}"
+
+  rm -f "${WEATHER_CACHE}"
+  local part1="$(sed '16q;d' "${WEATHER_FILE}" | grep -wo "[0-9]*%" | sort -n | sed -e '$!d' | sed -e "s/^/☔ /g" | tr -d '\n')"
+  local part2="$(sed '13q;d' "${WEATHER_FILE}" | grep -o "m\\(-\\)*[0-9]\\+" | sort -n -t 'm' -k 2n | sed -e 1b -e '$!d' | tr '\n|m' ' ' | awk '{print " ❄️",$1 "°","🌞",$2 "°"}')"
+  echo "${part1}${part2}" > "${WEATHER_CACHE}"
+}
+
+if [ "$(stat -c %y "${WEATHER_CACHE}" >/dev/null 2>&1 | awk '{print $1}')" != "$(date '+%Y-%m-%d')" ]
+then
+  getforecast
+fi
+
+if [ -f "${WEATHER_CACHE}" ]; then
+  WEATHER="$(cat "${WEATHER_CACHE}" | tr -d '\n')"
+else
+  WEATHER="-"
+fi
+
+xsetroot -name " ${WEATHER} • ${NETWORK_STATUS} • ${CPU_TEMP} • ${STATUS_ICON} ${BAT_CAPACITY}% • ${KEYBOARD_LAYOUT} • ${LOCAL_TIME} ${NY_TIME}"
