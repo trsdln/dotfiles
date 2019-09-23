4b4b47b80bae2891c4f7a6d74c9e228e9abe4f7e
Taras Dolynyuk <trsdln@gmail.com>
2019-09-23 22:13:44 +0300

    Improve screen lock script

--- linux/scripts/lock-screen.sh
+++ linux/scripts/lock-screen.sh
@@ -1,11 +1,54 @@
-#!/bin/sh
+#!/bin/bash
 
-# otherwise we get status line content of status line before lock/sleep
-# over next statusline refresh timeout
-statusline-update.sh
+# Provides fixed delay to give simple lockers a little bit of time to lock
+# the screen before the system goes the sleep.
+# Based on /usr/share/doc/xss-lock/transfer-sleep-lock-generic-delay.sh
 
-# we need to change keyboard layout to correct one otherwise
-# there is no ability to change it while slock is running
-setxkbmap -model thinkpad -layout us
+## CONFIGURATION ##############################################################
 
-slock
+# Command to start the locker (should not fork)
+locker="slock"
+
+# Delay in seconds. Note that by default systemd-logind allows a maximum sleep
+# delay of 5 seconds.
+sleep_delay=1
+
+# Run before starting the locker
+pre_lock() {
+  return
+}
+
+# Run after the locker exits
+post_lock() {
+  # otherwise we get status line content of status line before lock/sleep
+  # over next statusline refresh timeout
+  statusline-update.sh
+
+  # we need to change keyboard layout to correct one otherwise
+  # there is no ability to change it while slock is running
+  setxkbmap -model thinkpad -layout us
+  return
+}
+
+###############################################################################
+
+pre_lock
+
+# kill locker if we get killed
+trap 'kill %%' TERM INT
+
+if [[ -e /dev/fd/${XSS_SLEEP_LOCK_FD:--1} ]]; then
+  # lock fd is open, make sure the locker does not inherit a copy
+  $locker {XSS_SLEEP_LOCK_FD}<&- &
+
+  sleep $sleep_delay
+
+  # now close our fd (only remaining copy) to indicate we're ready to sleep
+  exec {XSS_SLEEP_LOCK_FD}<&-
+else
+  $locker &
+fi
+
+wait # for locker to exit
+
+post_lock
--- linux/scripts/toggle-keyboard-layout.sh
+++ linux/scripts/toggle-keyboard-layout.sh
@@ -4,8 +4,12 @@ KEYBOARD_LAYOUT="$(setxkbmap -query | awk '/layout/ {print $2}')"
 
 if [ "${KEYBOARD_LAYOUT}" = 'us' ]; then
   setxkbmap -model thinkpad -layout ua -variant legacy
+  # "legacy" variant fixes problem with ? vs . at UA keyboard
 else
+  # Set keyboard layout
   setxkbmap us -model thinkpad -layout us
+  # support for play/pause media keys
+  # xkbcomp $HOME/.dotfiles/linux/xkbmap.xkb $DISPLAY
 fi
 
 statusline-update.sh
