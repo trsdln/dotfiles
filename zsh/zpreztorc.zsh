906a13df4ab20aeac5d53a267c8c4d1dc21c3ee9
Taras Dolynyuk <trsdln@gmail.com>
2019-10-12 00:53:41 +0300

    Move zprezto dir away from home dir

--- zsh/zpreztorc.zsh
+++ zsh/zpreztorc.zsh
@@ -1,13 +1,13 @@
 export DOTFILES_PATH="${HOME}/.dotfiles"
 
 # Point Zprezto to configs directory
-ZDOTDIR="${DOTFILES_PATH}/zsh/zprezto"
+ZDOTDIR="${DOTFILES_PATH}/zsh/zdotdir"
 
 # Source zprofile, because by default zprezto does it
-source "${HOME}/.zprezto/runcoms/zprofile"
+source "${DOTFILES_PATH}/zsh/zprezto/runcoms/zprofile"
 
 # Source Prezto
-source "${HOME}/.zprezto/init.zsh"
+source "${DOTFILES_PATH}/zsh/zprezto/init.zsh"
 
 [ -f "${DOTFILES_PATH}/zsh/commonrc.zsh" ] && source "${DOTFILES_PATH}/zsh/commonrc.zsh"
 
