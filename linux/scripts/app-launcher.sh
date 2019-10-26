d1612dff1ce555610c814f7142caa4d96edbe388
Taras Dolynyuk <trsdln@gmail.com>
2019-10-27 00:43:30 +0300

    Port app launcher to dash

--- linux/scripts/app-launcher.sh
+++ linux/scripts/app-launcher.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 print_app_options () {
 echo "Google Chrome:chromium
@@ -17,10 +17,10 @@ Gimp:gimp"
 }
 
 prompt_app_and_run () {
-  local selected_option=$(print_app_options | cut -d ':' -f1 | dmenu -i -l 20 -p 'Launch App')
+  local selected_option="$(print_app_options | cut -d ':' -f1 | dmenu -i -l 20 -p 'Launch App')"
 
   if [ "${selected_option}" != "" ]; then
-    local run_cmd=$(print_app_options | grep "${selected_option}" | cut -d ':' -f2)
+    local run_cmd="$(print_app_options | grep "${selected_option}" | cut -d ':' -f2)"
 
     ${run_cmd} &
   fi
