6d5d049bceee8ee650e16fc4527a943bedb3c130
Taras Dolynyuk <trsdln@gmail.com>
2019-10-26 21:09:55 +0300

    Port easy-mount.sh to dash

--- linux/scripts/easy-mount.sh
+++ linux/scripts/easy-mount.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Based on http://ratfactor.com/slackware/mounty/
 
@@ -6,25 +6,24 @@
 # from array make choice,description pairs
 selected=$(lsblk -l -p -n --output name,size,label,vendor,model | dmenu -l 10 -p "Select device to mount:" | awk '{print $1}')
 
-if [[ -z $selected ]]
+if [ -z $selected ]
 then
 	echo "No device selected."
 	exit
 fi
 
 # take chosen selection and see if it's already mounted
-#if findmnt $selected >/dev/null
-if dir=$(findmnt --source $selected -n -o TARGET); then
-	echo "$selected already mounted at $dir"
+dir=$(findmnt --source $selected -n -o TARGET)
+if [ -z $dir ]; then
+  dir="/mnt"
+  echo "Mounting $selected at $dir..."
+  sudo mount $selected $dir || exit
 else
-	dir="/mnt"
-	echo "Mounting $selected at $dir..."
-	sudo mount $selected $dir || exit
+  echo "$selected already mounted at $dir"
 fi
 
 # now that it's mounted, call user-supplied arguments
 $1 $dir $2 $3 $4 $5
-#echo "running:" $1 $dir $2 $3 $4 $5
 
 unmount_resp=$(printf "No\nYes" | dmenu -p "Do you want unmount '$selected'?")
 
