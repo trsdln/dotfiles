a8e7de25370ab6a74b93c2b37bd44a1df3cef77f
Taras <trsdln@users.noreply.github.com>
2019-12-28 21:46:12 +0200

    Add additional mode at screen switching

--- linux/scripts/toggle-hdmi.sh
+++ linux/scripts/toggle-hdmi.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
 
-HDMI_OUT="HDMI1"
+# Supported modes:
+#  - only primary monitor:    [PRIMARY] [    -    ]
+#  - 2 side-by-side monitors: [PRIMARY] [SECONDARY]
+#  - only secondary monitor:  [   -   ] [SECONDARY]
 
-if xrandr --listmonitors | grep "$HDMI_OUT"; then
-  xrandr --output "$HDMI_OUT" --off
-  HDMI_STATUS="off"
+PRIMARY_OUTPUT="eDP1"
+SECONDARY_OUTPUT="HDMI1"
+
+SECONDARY_ENABLED=$(xrandr --listmonitors | grep "${SECONDARY_OUTPUT}")
+PRIMARY_ENABLED=$(xrandr --listmonitors | grep "${PRIMARY_OUTPUT}")
+
+if [ "${PRIMARY_ENABLED}" != "" ] && [ "${SECONDARY_ENABLED}" = "" ]; then
+  CURRENT_MODE="Only Secondary"
+  xrandr --output "${PRIMARY_OUTPUT}" --off \
+    --output "${SECONDARY_OUTPUT}" --auto
+elif [ "${PRIMARY_ENABLED}" = "" ] && [ "${SECONDARY_ENABLED}" != "" ]; then
+  CURRENT_MODE="Side-by-Side"
+  xrandr --output "${PRIMARY_OUTPUT}" --auto \
+    --output "${SECONDARY_OUTPUT}" --auto --right-of "${PRIMARY_OUTPUT}"
 else
-  xrandr --output "$HDMI_OUT" --mode 1920x1080 --right-of eDP1
-  HDMI_STATUS="on"
+  CURRENT_MODE="Only Primary"
+  xrandr --output "${PRIMARY_OUTPUT}" --auto \
+    --output "${SECONDARY_OUTPUT}" --off
 fi
 
 notify-send --hint=string:x-canonical-private-synchronous:hdmi-status \
   --icon=/usr/share/icons/Paper/32x32/devices/display.png \
-  "HDMI" "${HDMI_STATUS}"
+  "Screen Mode" "${CURRENT_MODE}"
