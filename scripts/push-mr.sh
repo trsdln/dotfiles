ff34bd9c58f804b37229f8b249b40c77e4172e0a
Taras <trsdln@gmail.com>
2020-03-25 12:04:53 +0200

    Improve push-mr script

--- scripts/push-mr.sh
+++ scripts/push-mr.sh
@@ -2,5 +2,10 @@
 
 # push with -u origin and open create MR link
 
-git push --no-verify -u origin $(git rev-parse --abbrev-ref HEAD) \
-  | grep 'https://gitlab.com' | awk '{print $2}' | xargs openfile
+open_create_mr_url() {
+  echo "$1"
+  openfile $1
+}
+
+git push --no-verify -u origin $(git rev-parse --abbrev-ref HEAD) 2>&1 \
+  | grep 'https://gitlab.com' | awk '{print $2}' | xargs open_create_mr_url
