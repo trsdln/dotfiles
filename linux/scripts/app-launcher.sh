12353a205b216e93cb90c2593b6111dfe61986e2
Taras <trsdln@gmail.com>
2020-04-06 15:12:53 +0300

    Added lf entry

--- linux/scripts/app-launcher.sh
+++ linux/scripts/app-launcher.sh
@@ -19,6 +19,7 @@ Chrome Incognito:chromium --incognito
 TMUX New:tmux_new
 TMUX Attach:tmux_attach
 PulseMixer:alacritty -e pulsemixer
+LF:alacritty -e lf
 Alacritty Terminal:alacritty
 Telegram:telegram-desktop
 Slack:slack
