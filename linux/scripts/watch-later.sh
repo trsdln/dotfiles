59620b41d0f13462768ee37609c3b8504669d14a
Taras <trsdln@gmail.com>
2020-06-05 23:46:40 +0300

    Added titles to watch later playlist

--- linux/scripts/watch-later.sh
+++ linux/scripts/watch-later.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-watch_later_file=$HOME/.local/share/watch-later.txt
+watch_later_file=$HOME/.local/share/watch-later.m3u
 
 cmd=$1
 
@@ -8,16 +8,18 @@ notification_title="Watch Later"
 notification_hint="string:x-canonical-private-synchronous:watch-later"
 
 if [ $cmd = "add" ]; then
+  title=$(youtube-dl --skip-download --get-title --no-warnings "$2")
+  echo "#EXTINF:, ${title}" >> $watch_later_file
   echo $2 >> $watch_later_file
   statusline-update.sh
-  notify-send --hint $notification_hint "$notification_title" "$2 added"
+  notify-send --hint $notification_hint "$notification_title" "'${title}' added"
 elif [ $cmd = "clear" ]; then
   rm -rf $watch_later_file
   statusline-update.sh
   notify-send --hint $notification_hint "$notification_title" "Cleaned!"
 elif [ $cmd = "play" ]; then
   notify-send --hint $notification_hint "$notification_title" "Starting mpv..."
-  exec mpv --playlist=$watch_later_file
+  exec mpv $watch_later_file
 elif [ $cmd = "count" ]; then
   items_count=$(wc -l $watch_later_file 2> /dev/null || echo 0)
   echo "$items_count" | awk '{print $1}'
