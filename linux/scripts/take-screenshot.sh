c6b9939c76ca3a6785bb25c5aa24b6865ce18b71
Taras <trsdln@gmail.com>
2020-06-16 15:27:03 +0300

    Fix screenshot script

--- linux/scripts/take-screenshot.sh
+++ linux/scripts/take-screenshot.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [ "$1" = "clipboard"]; then
+if [ "$1" = "clipboard" ]; then
   maim -s | xclip -selection clipboard -t image/png
 else
   screenshot_file="Desktop/screenshot_$(date +%d-%m-%y_%H-%M-%S).png"
   notify-send -h string:x-canonical-private-synchronous:take_screenshot \
-    "Screenshot" "Saving as $screenshot_file"
+    "Screenshot to $1" "Saving as $screenshot_file"
 
   maim -s > $HOME/$screenshot_file
 fi
