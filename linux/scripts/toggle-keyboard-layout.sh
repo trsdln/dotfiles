eaf2beb4f9af966a66c6d7968db3ba036cd737b8
Taras <trsdln@gmail.com>
2020-07-16 11:16:25 +0300

    Enable pc keyboard layout

--- linux/scripts/toggle-keyboard-layout.sh
+++ linux/scripts/toggle-keyboard-layout.sh
@@ -3,8 +3,7 @@
 KEYBOARD_LAYOUT="$(setxkbmap -query | awk '/layout/ {print $2}')"
 
 if [ "${KEYBOARD_LAYOUT}" = 'us' ]; then
-  setxkbmap -model thinkpad -layout ua -variant legacy
-  # "legacy" variant fixes problem with ? vs . at UA keyboard
+  setxkbmap -model thinkpad -layout ua
 else
   # Set keyboard layout
   setxkbmap us -model thinkpad -layout us
