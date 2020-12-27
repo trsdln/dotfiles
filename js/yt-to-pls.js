322a7480997a52e595030c5aedcadfa66cb262f5
Taras <trsdln@gmail.com>
2020-12-27 14:20:56 +0200

    Adjust playlist generator

--- js/yt-to-pls.js
+++ js/yt-to-pls.js
@@ -1,3 +1,7 @@
 res = [];
 document.querySelectorAll('a.ytd-grid-video-renderer').forEach(i => res.push([i.innerText,i.href]));
 res.map(([t,h]) =>`#EXTINF:, ${t}\n${h}`).join('\n');
+
+res = [];
+document.querySelectorAll('a.video').forEach(i => res.push([i.innerText,i.href]));
+res.map(([t,h]) =>`#EXTINF:, ${t}\n${h}`).join('\n');
