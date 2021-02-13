fc01659101d5730d3b5fdcc37a255866f335d549
Taras <trsdln@gmail.com>
2021-02-13 09:26:40 +0200

    Improve watch-later

--- linux/scripts/watch-later.sh
+++ linux/scripts/watch-later.sh
@@ -12,28 +12,39 @@ update_watch_later_widget() {
   $SCRIPTS_DIR/statusline/widgets/watch-later.sh
 }
 
-if [ $cmd = "add" ]; then
-  title=$(youtube-dl --skip-download --get-title --no-warnings "$2")
-  echo "#EXTINF:, ${title}" >> $watch_later_file
-  echo $2 >> $watch_later_file
-  update_watch_later_widget
-  notify-send --hint $notification_hint "$notification_title" "'${title}' added"
-elif [ $cmd = "clear" ]; then
-  rm -rf $watch_later_file
-  update_watch_later_widget
-  notify-send --hint $notification_hint "$notification_title" "Cleaned!"
-elif [ $cmd = "play" ]; then
-  notify-send --hint $notification_hint "$notification_title" "Starting mpv..."
-  exec mpv $watch_later_file
-elif [ $cmd = "count" ]; then
-  items_count=$(wc -l $watch_later_file 2> /dev/null)
-  # strip file name
-  items_count=${items_count%% *}
-  # fall back to 0 as default
-  items_count=${items_count:-0}
-  # account for video titles
-  items_count=$((${items_count} / 2))
-  echo "$items_count"
-elif [ $cmd = "edit" ]; then
-  $TERMINAL -e $EDITOR $watch_later_file
-fi
+case $cmd in
+  add)
+    title=$(youtube-dl --skip-download --get-title --no-warnings "$2")
+    echo "#EXTINF:, ${title}" >> $watch_later_file
+    echo $2 >> $watch_later_file
+    update_watch_later_widget
+    notify-send --hint $notification_hint "$notification_title" "'${title}' added"
+    ;;
+  clear)
+    rm -rf $watch_later_file
+    update_watch_later_widget
+    notify-send --hint $notification_hint "$notification_title" "Cleaned!"
+    ;;
+  edit)
+    $TERMINAL -e $EDITOR $watch_later_file
+    update_watch_later_widget
+    ;;
+  play)
+    notify-send --hint $notification_hint "$notification_title" "Starting mpv..."
+    exec mpv $watch_later_file
+    ;;
+  count)
+    items_count=$(wc -l $watch_later_file 2> /dev/null)
+    # strip file name
+    items_count=${items_count%% *}
+    # fall back to 0 as default
+    items_count=${items_count:-0}
+    # account for video titles
+    items_count=$((${items_count} / 2))
+    echo "$items_count"
+    ;;
+  *)
+    echo "Unknown command '${cmd}'"
+    exit 1
+    ;;
+esac
